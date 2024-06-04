97dc7b0a5e6c3f4d9af4e2d7d473fa4abcc7c3db
Jirapat <jirapat.du24@gmail.com>
2024-06-05 00:14:04 +0700

    Exercise #1 : Strict and Same

---
+++ ex-1.js
@@ -0,0 +1,18 @@
+// Exercise #1 Strict and Same
+// Start coding here
+Object.is(10, 20); // false
+Object.is(0, -0); // false
+Object.is(NaN, 100); // false
+Object.is("one", "two"); // false
+Object.is({ name: "John", age: 30 }, { name: "John", age: 30 }); // true
+Object.is(null, undefined); // false
+
+10 === 20; // false
+3.14 !== 3.14; // false
+"TechUp" !== "TechUp"; // false
+NaN === NaN;
+0 !== -0; // false
+
+let objectA = { course: "Web Developer", price: 300000 };
+let objectB = { course: "Web Developer", price: 300000 };
+objectA === objectB; // true
